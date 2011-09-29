7759c4fcb18a89558c2ec843087f2430597e62c6
caminadab <caminadab@gmail.com>
2011-09-29 04:27:40 +0000

    Adicionado funcoes de mapeamento do tabuleiro, usando uma biblioteca de GUI chamada PGU para criar botoes e etc...

---
+++ peca.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os, sys
+import pygame
+
+class Peca:
+    imgPath = [os.path.join("img","pecavermelha.png"), os.path.join("img","pecapreta.png")]
+    imgSurface = [pygame.image.load(imgPath[0]),pygame.image.load(imgPath[1])]
+    estado = 0 #0 = vazio, 1 = jogador vermelho, 2 = jogador preto
+    
+    def __init__(self):
+        self.estado = 0
+        
+    def img(self):
+        if (self.estado != 0):
+            return self.imgSurface[self.estado-1]
+        else:
+            return ""
+        
+    def __str__(self):
+        return "Peca com estado " + str(self.estado)
+    
+    def __repr__(self):
+        return "Peca com estado " + str(self.estado)
--- tabuleiro.py
+++ tabuleiro.py
@@ -1,23 +1,62 @@
 #!/usr/bin/env python
-import os, sys
+import os, sys, copy
+from peca import *
 
 class Tabuleiro:
     pygame = 0
+    ImgSurface = 0
+    offset = (0,30)
+    size = (341,341)
     tabuleiro = []
+    IMGPATH = os.path.join("img","tabuleiro.png")
+
+    last = 2
     
     def __init__(self, game):
-        self.pygame = game
-        screen = game.display.get_surface()
-        tabuleiroImgPath = os.path.join("img","tabuleiro.png")
-        tabuleiroSurface = game.image.load(tabuleiroImgPath)
-        screen.blit(tabuleiroSurface, (0,0))
-        game.display.flip()
+        self.pygame = game       
+        self.ImgSurface = self.pygame.image.load(self.IMGPATH)
         row = [0]*8
         
         for item in row:
-            self.tabuleiro.append(row)
+            self.tabuleiro.append(copy.deepcopy(row))
+            
+        self.initPecas()
         
     def refresh(self):
-        self.pygame.display.flip()
+        screen = self.pygame.display.get_surface()        
+        screen.blit(self.ImgSurface, self.offset)
         
+        for i in range(8):
+            for j in range(8):
+                item = self.tabuleiro[i][j]
+                if item.estado != 0:
+                    newsurface = item.img()
+                    screen.blit(newsurface, (i*41 + 5 + self.offset[0], j*41 + 5 + self.offset[1]))
+        self.pygame.display.flip()        
+        
+    def initPecas(self):
+        for i in range(8):
+            for j in range (8):
+                self.tabuleiro[i][j] = Peca()
     
+    def click(self, x, y):
+        if x < self.offset[0] or x > self.size[0] + self.offset[0]:
+            return
+        if y < self.offset[1] or y > self.size[1] + self.offset[1]:
+            return
+        peca = self.tabuleiro[self.map(x, 0)][self.map(y, 1)]
+        if peca.estado != 0:
+            peca.estado = 0
+        else:
+            peca.estado = self.alternador()
+
+        
+    def map(self, x, i):
+        return int(round((x-self.offset[i])/41,0))
+        
+    def alternador(self):
+        if self.last == 1:
+            self.last = 2
+        else:
+            self.last = 1
+        return self.last
