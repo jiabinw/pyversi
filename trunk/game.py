12dbc2989a8ca9d41934b4936be40b671684212c
caminadab@gmail.com <caminadab@gmail.com@67e9baab-538c-38da-8e11-d0b489e7b102>
2011-09-29 04:27:40 +0000

    Adicionado funcoes de mapeamento do tabuleiro, usando uma biblioteca de GUI chamada PGU para criar botoes e etc...

--- trunk/game.py
+++ trunk/game.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python
 import os, sys
-import pygame
 from tabuleiro import *
+import pygame
 from pygame.locals import *
+from pgu import gui
 
 class Game:
+    tab = ''
     
     def input(self,events): 
            for event in events: 
-              if event.type == QUIT: 
-                 sys.exit(0) 
-              else:
-                 print event
+                if event.type == QUIT: 
+                    sys.exit(0)
+                elif event.type == MOUSEBUTTONDOWN:
+                    self.tab.click(event.dict['pos'][0],event.dict['pos'][1])
+                else:
+                    self.app.event(event)
     
     def __init__(self):
         pygame.init()
-        window = pygame.display.set_mode((329, 329))
+        window = pygame.display.set_mode((329, 359),SWSURFACE)
         pygame.display.set_caption('Reversi')
-        tab = Tabuleiro(pygame)
-                 
+        
+        formf=gui.Form()
+        self.app = gui.App()
+        form = gui.Table()
+        form.tr()
+        e = gui.Button("Novo Jogo")
+        form.td(e)
+        
+        c = gui.Container(align=-1,valign=-1)
+        c.add(form,0,0)
+        
+        self.app.init(c)
+        self.tab = Tabuleiro(pygame)      
     
-        while True: 
-           self.input(pygame.event.get())
-           tab.refresh()
+        while True:
+            self.app.paint()
+            pygame.display.flip()
+            self.input(pygame.event.get())
+            self.tab.refresh()
 
-   
-myGame = Game()
+game = Game()
+
