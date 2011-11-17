9e842ac5ffe916ef0700af2efe75ef50c3ca71e7
stallin.1991@gmail.com <stallin.1991@gmail.com@67e9baab-538c-38da-8e11-d0b489e7b102>
2011-11-17 02:25:56 +0000

    Verificar jogada, e flip

--- trunk/tabuleiro.py
+++ trunk/tabuleiro.py
@@ -83,21 +83,172 @@ class Tabuleiro:
         for i in range(8):
             for j in range (8):
                 self.tabuleiro[i][j] = Peca()
+	
+	pecas = [self.tabuleiro[3][3], self.tabuleiro[4][3], self.tabuleiro[4][4], self.tabuleiro[3][4]]
+
+	for peca in pecas:
+            peca.estado = self.alternador()
+
+	self.pontuacao()
     
     def click(self, x, y):
+
         if x < self.offset[0] or x > self.size[0] + self.offset[0]:
             return
         if y < self.offset[1] or y > self.size[1] + self.offset[1]:
             return
 
         peca = self.tabuleiro[self.map(x, 0)][self.map(y, 1)]
-        if peca.estado == 0:
+
+        if peca.estado == 0 and self.jogadaValida(self.map(x, 0), self.map(y, 1)) == 1:
             peca.estado = self.alternador()
         else:
             peca.estado = peca.estado
         self.fimJogo()
         self.pontuacao()
         
+
+    def jogadaValida(self, i, j): #0:jogadaInvalida, 1:jogadaValida	
+	todosVirar = []
+	virar = []
+	jogadaValida = 0
+	# PERCORRENDO COLUNAS
+	for a in range(8 - i - 1): # Para direita
+		col = i + a + 1
+		peca = self.tabuleiro[col][j]
+		resposta = self.verificaJogada(peca, a)
+		if resposta == 1:
+			todosVirar.append(virar)
+			jogadaValida = 1
+			break
+		if resposta == 2:
+			break
+		virar.append(peca)
+
+	virar = []
+	for a in range(i): # Para esquerda
+		col = i - (a + 1)
+		peca = self.tabuleiro[col][j]
+		resposta = self.verificaJogada(peca, a)
+		if resposta == 1:
+			todosVirar.append(virar)
+			jogadaValida = 1
+			break
+		if resposta == 2:
+			break
+		virar.append(peca)
+	
+	virar = []
+	# PERCORRENDO LINHAS
+	for a in range(8 - j - 1): # Para baixo
+		lin = j + a + 1
+		peca = self.tabuleiro[i][lin]
+		resposta = self.verificaJogada(peca, a)
+		if resposta == 1:
+			todosVirar.append(virar)
+			jogadaValida = 1
+			break
+		if resposta == 2:
+			break
+		virar.append(peca)
+	virar = []
+	for a in range(j): # Para cima
+		lin = j - (a + 1)
+		peca = self.tabuleiro[i][lin]
+		resposta = self.verificaJogada(peca, a)
+		if resposta == 1:
+			todosVirar.append(virar)
+			jogadaValida = 1
+			break
+		if resposta == 2:
+			break
+		virar.append(peca)
+
+	# PERCORRENDO DIAGONAIS
+	if j > i: # A diogonal será percorrida até no maximo a distancia mais proxima a borda (horizontal ou vertical)
+		indD = 8 - j - 1
+	else:
+		indD = 8 - i - 1
+	virar = []
+	for a in range(indD): # Para baixo, direita
+		lin = j + a + 1
+		col = i + a + 1
+		peca = self.tabuleiro[col][lin]
+		resposta = self.verificaJogada(peca, a)
+		if resposta == 1:
+			todosVirar.append(virar)
+			jogadaValida = 1
+			break
+		if resposta == 2:
+			break
+		virar.append(peca)
+	virar = []
+	for a in range(indD): # Para cima, direita
+		lin = j - (a + 1)
+		col = i + a + 1
+		peca = self.tabuleiro[col][lin]
+		resposta = self.verificaJogada(peca, a)
+		if resposta == 1:
+			todosVirar.append(virar)
+			jogadaValida = 1
+			break
+		if resposta == 2:
+			break
+		virar.append(peca)
+	virar = []
+	for a in range(indD): # Para baixo, esquerda
+		lin = j + a + 1
+		col = i - (a + 1)
+		peca = self.tabuleiro[col][lin]
+		resposta = self.verificaJogada(peca, a)
+		if resposta == 1:
+			todosVirar.append(virar)
+			jogadaValida = 1
+			break
+		if resposta == 2:
+			break
+		virar.append(peca)
+	virar = []
+	for a in range(indD): # Para cima, esquerda
+		lin = j - (a + 1)
+		col = i - (a + 1)
+		peca = self.tabuleiro[col][lin]
+		resposta = self.verificaJogada(peca, a)
+		if resposta == 1:
+			todosVirar.append(virar)
+			jogadaValida = 1
+			break
+		if resposta == 2:
+			break
+		virar.append(peca)
+
+	# Se a jogada for valida, da o flip nas pecas para a cor do jogador corrente
+	if jogadaValida == 1:
+		if self.last == 1:
+			cor = 2
+		else:
+			cor = 1
+		for t in todosVirar:
+			for t0 in t:
+				t0.estado = cor
+	else:
+		print "Erro: Jogada Invalida"		
+		
+	return jogadaValida
+
+    def verificaJogada(self, peca, a): # 1:jogadaValida, 0:continua, 2:para
+	if peca.estado == 0:
+		print " PARAR "
+		return 2
+
+	# Se a peca que esta percorrendo e a mesma do jogador atual e ha pelo menos uma peca entre as duas
+	if peca.estado != self.last:
+		if a > 0:
+			return 1
+		else:
+			return 2
+
+	return 0
 
     def pontuacao(self):
         self.pontosVermelho = 0
