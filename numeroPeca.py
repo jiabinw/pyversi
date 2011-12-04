d80f7b44a61b5dfc469a9e149f5015a9db313fc0
paolobns <paolobns@gmail.com>
2011-12-04 02:46:21 +0000

    Ah muleke

---
+++ numeroPeca.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os, sys, copy
+from peca import *
+from tabuleiro import *
+
+class NumeroPeca:
+  
+    maxPontuacao = 0
+    melhorJogada = [0,0]
+  
+    # Entrada o tabuleiro e qual o eh o jogador atual
+    def __init__(self, tabuleiro, jogadorAtual):	
+	
+        for i in range(8):
+            for j in range(8):
+	      novoTab = copy.deepcopy(tabuleiro)
+	      peca = novoTab[i][j]	      
+	      novoTab.jogadaValida(i, j, novoTab, 0)
+	      novoTab.pontuacao
+	      
+	      if jogadorAtual == 1 :
+		if novoTab.pontosVermelho > maxPontuacao:
+		  maxPontuacao = novoTab.pontosVermelho
+		  melhorJogada = [i,j];
+	      else
+		if novoTab.pontosPreto > maxPontuacao:
+		  maxPontuacao = novoTab.pontosPreto
+		  melhorJogada = [i,j];
+		
+                
+             
+	return melhorJogada
+      
+      
+      
