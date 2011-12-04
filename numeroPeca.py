eba5f83398cec4aed32b3e0888e8ede91c1ed47d
paolobns <paolobns@gmail.com>
2011-12-04 04:00:46 +0000

    Justo do It

--- numeroPeca.py
+++ numeroPeca.py
@@ -3,20 +3,20 @@ import os, sys, copy
 from peca import *
 from tabuleiro import *
 
-class NumeroPeca:
-  
-    contador = 0
+class NumeroPeca:   
     
     # Entrada o tabuleiro e qual o eh o jogador atual
     def calcula(self, tabuleiro, jogadorAtual):	
+    
+	contador = 0
 	
         for i in range(8):
             for j in range(8):
 	      peca = tabuleiro[i][j]
 	      if peca.estado == jogadorAtual :
-		contador += contador	
+		contador += 1	
 		
-         contador = contador*100/64       
+	contador = (contador / float(64)) * 100
              
 	return contador
       
